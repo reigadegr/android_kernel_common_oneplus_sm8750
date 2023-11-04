e583432d0d02a70f25b983573fddf868d6132021
Peter Zijlstra <peterz@infradead.org>
2023-11-04 11:59:21 +0100

    sched/deadline: Introduce deadline servers
    Low priority tasks (e.g., SCHED_OTHER) can suffer starvation if tasks
    with higher priority (e.g., SCHED_FIFO) monopolize CPU(s).
    
    RT Throttling has been introduced a while ago as a (mostly debug)
    countermeasure one can utilize to reserve some CPU time for low priority
    tasks (usually background type of work, e.g. workqueues, timers, etc.).
    It however has its own problems (see documentation) and the undesired
    effect of unconditionally throttling FIFO tasks even when no lower
    priority activity needs to run (there are mechanisms to fix this issue
    as well, but, again, with their own problems).
    
    Introduce deadline servers to service low priority tasks needs under
    starvation conditions. Deadline servers are built extending SCHED_DEADLINE
    implementation to allow 2-level scheduling (a sched_deadline entity
    becomes a container for lower priority scheduling entities).
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Daniel Bristot de Oliveira <bristot@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/4968601859d920335cf85822eb573a5f179f04b8.1699095159.git.bristot@kernel.org

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -66,11 +66,13 @@ struct root_domain;
 struct rq;
 struct sched_attr;
 struct sched_param;
+struct sched_dl_entity;
 struct seq_file;
 struct sighand_struct;
 struct signal_struct;
 struct task_delay_info;
 struct task_group;
+struct task_struct;
 struct user_event_mm;
 
 #ifdef CONFIG_HMBIRD_SCHED
@@ -598,6 +600,9 @@ struct sched_rt_entity {
 	ANDROID_KABI_RESERVE(4);
 } __randomize_layout;
 
+typedef bool (*dl_server_has_tasks_f)(struct sched_dl_entity *);
+typedef struct task_struct *(*dl_server_pick_f)(struct sched_dl_entity *);
+
 struct sched_dl_entity {
 	struct rb_node			rb_node;
 
@@ -645,6 +650,7 @@ struct sched_dl_entity {
 	unsigned int			dl_yielded        : 1;
 	unsigned int			dl_non_contending : 1;
 	unsigned int			dl_overrun	  : 1;
+	unsigned int			dl_server         : 1;
 
 	/*
 	 * Bandwidth enforcement timer. Each -deadline task has its
@@ -659,7 +665,20 @@ struct sched_dl_entity {
 	 * timer is needed to decrease the active utilization at the correct
 	 * time.
 	 */
-	struct hrtimer inactive_timer;
+	struct hrtimer			inactive_timer;
+
+	/*
+	 * Bits for DL-server functionality. Also see the comment near
+	 * dl_server_update().
+	 *
+	 * @rq the runqueue this server is for
+	 *
+	 * @server_has_tasks() returns true if @server_pick return a
+	 * runnable task.
+	 */
+	struct rq			*rq;
+	dl_server_has_tasks_f		server_has_tasks;
+	dl_server_pick_f		server_pick;
 
 #ifdef CONFIG_RT_MUTEXES
 	/*
@@ -786,6 +805,7 @@ struct task_struct {
 	struct sched_entity		se;
 	struct sched_rt_entity		rt;
 	struct sched_dl_entity		dl;
+	struct sched_dl_entity		*dl_server;
 	const struct sched_class	*sched_class;
 
 #ifdef CONFIG_SCHED_CORE
