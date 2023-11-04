8c948fc2310b93321cd56d142230514eab8fe83a
Peter Zijlstra <peterz@infradead.org>
2023-11-04 11:59:18 +0100

    sched: Unify runtime accounting across classes
    All classes use sched_entity::exec_start to track runtime and have
    copies of the exact same code around to compute runtime.
    
    Collapse all that.
    
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Daniel Bristot de Oliveira <bristot@kernel.org>
    Link: https://lkml.kernel.org/r/20220221111749.1928222-1-cgel.zte@gmail.com
    Signed-off-by: Guo Ziliang <guo.ziliang@zte.com.cn>
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Reviewed-by: Ran Xiaokai <ran.xiaokai@zte.com.cn>
    Reviewed-by: Jiang Xuexin <jiang.xuexin@zte.com.cn>
    Reviewed-by: Yang Yang <yang.yang29@zte.com.cn>
    Acked-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2279,6 +2279,13 @@ struct affinity_context {
 	unsigned int flags;
 };
 
+enum rq_onoff_reason {
+	RQ_ONOFF_HOTPLUG,               /* CPU is going on/offline */
+	RQ_ONOFF_TOPOLOGY,              /* sched domain topology update */
+};
+
+extern s64 update_curr_common(struct rq *rq);
+
 struct sched_class {
 
 #ifdef CONFIG_UCLAMP_TASK
@@ -3332,16 +3339,6 @@ extern int preempt_dynamic_mode;
 extern int sched_dynamic_mode(const char *str);
 extern void sched_dynamic_update(int mode);
 #endif
-
-static inline void update_current_exec_runtime(struct task_struct *curr,
-						u64 now, u64 delta_exec)
-{
-	curr->se.sum_exec_runtime += delta_exec;
-	account_group_exec_runtime(curr, delta_exec);
-
-	curr->se.exec_start = now;
-	cgroup_account_cputime(curr, delta_exec);
-}
 
 #ifdef CONFIG_SCHED_MM_CID
 
