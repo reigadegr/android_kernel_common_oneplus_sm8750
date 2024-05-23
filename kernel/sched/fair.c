8514ab1f3d1309de75c3ce7fdc7cb55618ae6528
Peter Zijlstra <peterz@infradead.org>
2024-05-23 11:00:10 +0200

    sched/fair: Assert {set_next,put_prev}_entity() are properly balanced
    Just a little sanity test..
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105029.486423066@infradead.org

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5499,6 +5499,7 @@ void set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	}
 
 	update_stats_curr_start(cfs_rq, se);
+	SCHED_WARN_ON(cfs_rq->curr);
 	cfs_rq->curr = se;
 
 	/*
@@ -5561,6 +5562,7 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 		/* in !on_rq case, update occurred at dequeue */
 		update_load_avg(cfs_rq, prev, 0);
 	}
+	SCHED_WARN_ON(cfs_rq->curr != prev);
 	cfs_rq->curr = NULL;
 }
 
