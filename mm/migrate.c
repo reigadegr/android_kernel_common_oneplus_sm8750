0bfa060143af4defdba53d006bff9ceaea77cd3d
pswbuild <pswbuild@oppo.com>
2024-11-13 15:34:17 +0800

    Synchronize code for OnePlus PJZ110_15.0.0.125(CN01) Based on QCOM release TAG:AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.041

--- mm/migrate.c
+++ mm/migrate.c
@@ -54,6 +54,9 @@
 #include <asm/tlbflush.h>
 
 #include <trace/events/migrate.h>
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/mm.h>
+#include <trace/hooks/vmscan.h>
 
 #undef CREATE_TRACE_POINTS
 #include <trace/hooks/mm.h>
@@ -657,6 +660,8 @@ void folio_migrate_flags(struct folio *newfolio, struct folio *folio)
 	/* page->private contains hugetlb specific flags */
 	if (!folio_test_hugetlb(folio))
 		folio->private = NULL;
+
+	trace_android_vh_look_around_migrate_folio(folio, newfolio);
 
 	/*
 	 * If any waiters have accumulated on the new page then
