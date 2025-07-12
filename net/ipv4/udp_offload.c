ab0296966ed6efee6aa8d8f4be505b1514f8a781
reigadegr <272268642@qq.com>
2025-07-12 16:06:41 +0800

    net/ipv4/udp_offload.c: Stop linearization to increase performance.

--- net/ipv4/udp_offload.c
+++ net/ipv4/udp_offload.c
@@ -355,10 +355,6 @@ struct sk_buff *__udp_gso_segment(struct sk_buff *gso_skb,
 			if (skb_pagelen(gso_skb) - sizeof(*uh) == skb_shinfo(gso_skb)->gso_size)
 				return __udp_gso_segment_list(gso_skb, features, is_ipv6);
 
-		ret = __skb_linearize(gso_skb);
-		if (ret)
-			return ERR_PTR(ret);
-
 		 /* Setup csum, as fraglist skips this in udp4_gro_receive. */
 		gso_skb->csum_start = skb_transport_header(gso_skb) - gso_skb->head;
 		gso_skb->csum_offset = offsetof(struct udphdr, check);
