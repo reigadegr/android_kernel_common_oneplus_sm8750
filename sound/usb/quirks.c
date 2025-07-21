3903bcca9ecb5bd5785e9adb6cfc72122bc360c6
W9015191 <v-zhangke4@oppo.com>
2025-07-21 19:47:23 +0800

    [BSP.CHG.Basic][(1/1)]{OS15.0.1 SM8750 LTS升级_05，解决冲突}
    适用范围：{V/qcom/kernel-6.6}
    准入id：{OPPO-9366762}
    分析：{LTS Kernel升级原因：Google要求，会通过CTS强管控，
    dodge当前使用kernek版本为6.6.30，即将过期，需升级到6.6.57}
    方案：{升级kernel，解决冲突}
    风险及影响[快/稳/省/功能/安全隐私]：{无}
    测试建议：{编译pass}
    跨组依赖(topic name)：{无}
    
    Change-Id: Icc5d0083be7dabc933bc5f2aa5f3c9b517080402
    Reviewed-on: http://gerrit.scm.adc.com:8080/38703119
    almid-check: cqc <oppo_codecheck@oppo.com>
    rules-check: pswbuild <pswbuild@oppo.com>
    Reviewed-by: 张柯-W9015191 <v-zhangke4@oppo.com>
    code-check: cqc <oppo_codecheck@oppo.com>
    Reviewed-by: 王靖淳-80036189 <wjc@oppo.com>
    oppo-check: cqc <oppo_codecheck@oppo.com>
    Tested-by: 王靖淳-80036189 <wjc@oppo.com>

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -555,7 +555,7 @@ int snd_usb_create_quirk(struct snd_usb_audio *chip,
 static int snd_usb_extigy_boot_quirk(struct usb_device *dev, struct usb_interface *intf)
 {
 	struct usb_host_config *config = dev->actconfig;
-	struct usb_device_descriptor new_device_descriptor;
+	struct usb_device_descriptor *new_device_descriptor __free(kfree) = NULL;
 	int err;
 
 	if (le16_to_cpu(get_cfg_desc(config)->wTotalLength) == EXTIGY_FIRMWARE_SIZE_OLD ||
@@ -574,11 +574,11 @@ static int snd_usb_extigy_boot_quirk(struct usb_device *dev, struct usb_interfac
 				new_device_descriptor, sizeof(*new_device_descriptor));
 		if (err < 0)
 			dev_dbg(&dev->dev, "error usb_get_descriptor: %d\n", err);
-		if (new_device_descriptor.bNumConfigurations > dev->descriptor.bNumConfigurations)
+		if (new_device_descriptor->bNumConfigurations > dev->descriptor.bNumConfigurations)
 			dev_dbg(&dev->dev, "error too large bNumConfigurations: %d\n",
-				new_device_descriptor.bNumConfigurations);
+				new_device_descriptor->bNumConfigurations);
 		else
-			memcpy(&dev->descriptor, &new_device_descriptor, sizeof(dev->descriptor));
+			memcpy(&dev->descriptor, new_device_descriptor, sizeof(dev->descriptor));
 		err = usb_reset_configuration(dev);
 		if (err < 0)
 			dev_dbg(&dev->dev, "error usb_reset_configuration: %d\n", err);
