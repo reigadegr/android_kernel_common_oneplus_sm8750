d85853ba077bea8c754236fb23ebe15729a2e1be
brokestar233 <3765589194@qq.com>
2025-08-03 21:21:18 +0800

    Revert "ANDROID: Track per-process dmabuf RSS"
    This reverts commit 5737a2075bd97dfd4b643fdb33cc49193b2c2d95.

--- kernel/fork.c
+++ kernel/fork.c
@@ -102,7 +102,6 @@
 #include <linux/iommu.h>
 #include <linux/tick.h>
 #include <linux/cpufreq_times.h>
-#include <linux/dma-buf.h>
 
 #ifdef CONFIG_USER_NS
 #include <linux/user_namespace.h>
@@ -1010,7 +1009,7 @@ void __put_task_struct(struct task_struct *tsk)
 	WARN_ON(refcount_read(&tsk->usage));
 	WARN_ON(tsk == current);
 
-	put_dmabuf_info(tsk);
+	sched_ext_free(tsk);
 	io_uring_free(tsk);
 	cgroup_free(tsk);
 	task_numa_free(tsk, true);
@@ -1206,7 +1205,6 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	tsk->task_frag.page = NULL;
 	tsk->wake_q.next = NULL;
 	tsk->worker_private = NULL;
-	tsk->dmabuf_info = NULL;
 
 	kcov_task_init(tsk);
 	kmsan_task_create(tsk);
@@ -2521,16 +2519,10 @@ __latent_entropy struct task_struct *copy_process(
 	p->bpf_ctx = NULL;
 #endif
 
-	retval = copy_dmabuf_info(clone_flags, p);
-	if (retval) {
-		pr_err("failed to copy dmabuf accounting info, err %d\n", retval);
-		goto bad_fork_cleanup_policy;
-	}
-
 	/* Perform scheduler related setup. Assign this task to a CPU. */
 	retval = sched_fork(clone_flags, p);
 	if (retval)
-		goto bad_fork_cleanup_dmabuf;
+		goto bad_fork_cleanup_policy;
 
 	retval = perf_event_init_task(p, clone_flags);
 	if (retval)
@@ -2850,8 +2842,8 @@ bad_fork_cleanup_audit:
 	audit_free(p);
 bad_fork_cleanup_perf:
 	perf_event_free_task(p);
-bad_fork_cleanup_dmabuf:
-	put_dmabuf_info(p);
+bad_fork_sched_cancel_fork:
+	sched_cancel_fork(p);
 bad_fork_cleanup_policy:
 	lockdep_free_task(p);
 #ifdef CONFIG_NUMA
