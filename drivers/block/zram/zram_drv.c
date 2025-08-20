Ardiwp/lunarkernel_xiaomi_sm8250

zram: 优化 zram_bio_discard 的性能 - 跳过非对齐页面和未分配页面的 TRIM 请求 - 减少锁竞争 Signed-off-by: Cloud_Yun <1770669041@qq.com>

drivers/block/zram/zram_drv.c
@@ -1586,6 +1586,7 @@ static void zram_bio_discard(struct zram *zram, u32 index,
 			     int offset, struct bio *bio)
 {
 	size_t n = bio->bi_iter.bi_size;
+	u64 freed_pages = 0;
 
 	/*
 	 * zram manages data in physical block size units. Because logical block
@@ -1605,13 +1606,17 @@ static void zram_bio_discard(struct zram *zram, u32 index,
 		index++;
 	}
 
-	while (n >= PAGE_SIZE) {
+    for (; n >= PAGE_SIZE; n -= PAGE_SIZE, index++) {
+		if (!zram_get_entry(zram, index))
+		    continue;
+
 		zram_slot_lock(zram, index);
-		zram_free_page(zram, index);
+		if (zram_get_entry(zram, index)) {
+			zram_free_page(zram, index);
+			atomic64_inc(&zram->stats.notify_free);
+			freed_pages++;
+		}
 		zram_slot_unlock(zram, index);
-		atomic64_inc(&zram->stats.notify_free);
-		index++;
-		n -= PAGE_SIZE;
 	}
 }
 

reigadegr/android_kernel_common_oneplus_sm8750

drivers/block/zram/zram_drv.c
@@ -1868,6 +1868,7 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 	u32 index = bio->bi_iter.bi_sector >> SECTORS_PER_PAGE_SHIFT;
 	u32 offset = (bio->bi_iter.bi_sector & (SECTORS_PER_PAGE - 1)) <<
 			SECTOR_SHIFT;
+	u64 freed_pages = 0;
 
 	/*
 	 * zram manages data in physical block size units. Because logical block
@@ -1887,13 +1888,17 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 		index++;
 	}
 
-	while (n >= PAGE_SIZE) {
+    for (; n >= PAGE_SIZE; n -= PAGE_SIZE, index++) {
+		if (!zram_get_handle(zram, index))
+		    continue;
+
 		zram_slot_lock(zram, index);
-		zram_free_page(zram, index);
+		if (zram_get_handle(zram, index)) {
+			zram_free_page(zram, index);
+			atomic64_inc(&zram->stats.notify_free);
+			freed_pages++;
+		}
 		zram_slot_unlock(zram, index);
-		atomic64_inc(&zram->stats.notify_free);
-		index++;
-		n -= PAGE_SIZE;
 	}
 
 	bio_endio(bio);