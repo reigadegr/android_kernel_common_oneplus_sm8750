ab49ba3a0fced583fe99b9d8a7c564237b03d6a9
reigadegr <2722688642@qq.com>
2025-08-30 19:16:13 +0800

    sched: fix: slot confilt2

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1549,18 +1549,18 @@ struct task_struct {
 #ifdef CONFIG_SLIM_SCHED
 	ANDROID_KABI_USE(1, unsigned long sched_prop);
 	ANDROID_KABI_USE(2, struct sched_ext_entity *scx);
-	ANDROID_KABI_USE(4, struct task_dma_buf_info *dmabuf_info);
+	ANDROID_KABI_USE(3, struct task_dma_buf_info *dmabuf_info);
 #else
 	ANDROID_KABI_USE(1, struct task_dma_buf_info *dmabuf_info);
 	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_RESERVE(3);
 #endif
 
 #ifdef CONFIG_QOS_CTRL
-	ANDROID_KABI_USE(3, struct qos_task_struct *qts);
+	ANDROID_KABI_USE(4, struct qos_task_struct *qts);
 #else
-	ANDROID_KABI_RESERVE(3);
-#endif
 	ANDROID_KABI_RESERVE(4);
+#endif
 	ANDROID_KABI_RESERVE(5);
 	ANDROID_KABI_RESERVE(6);
 	ANDROID_KABI_RESERVE(7);
