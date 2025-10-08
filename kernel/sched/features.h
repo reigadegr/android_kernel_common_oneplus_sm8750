cc203215d28ecb3fdb4566ed91e4100efa7b2462
brokestar233 <3765589194@qq.com>
2025-10-09 01:09:35 +0800

    sched/fair: Temporarily disable DELAY_DEQUEUE
    Enabling DELAY_DEQUEUE causes a bootloop. I suspect there's an issue with the DELAY_DEQUEUE logic, so I've temporarily disabled it. I'll re-enable it once I've identified and resolved the problem.
    
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -108,6 +108,6 @@ SCHED_FEAT(PLACE_REL_DEADLINE, true)
  *
  * DELAY_ZERO clips the lag on dequeue (or wakeup) to 0.
  */
-SCHED_FEAT(DELAY_DEQUEUE, true)
+SCHED_FEAT(DELAY_DEQUEUE, false)
 SCHED_FEAT(DELAY_ZERO, true)
 #endif
