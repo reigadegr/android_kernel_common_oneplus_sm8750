90a6e38f8d57e7764b6d2a55c2dd94c0cdbed698
brokestar233 <3765589194@qq.com>
2025-10-08 08:14:16 +0800

    [HACK] sched: fixup sched_entity to avoid ABI breakage
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -801,7 +801,7 @@ static inline void se_update_runnable(struct sched_entity *se)
 
 static inline long se_runnable(struct sched_entity *se)
 {
-	if (se->sched_delayed)
+	if (se->ext.sched_delayed)
 		return false;
 
 	if (entity_is_task(se))
@@ -817,7 +817,7 @@ static inline void se_update_runnable(struct sched_entity *se) {}
 
 static inline long se_runnable(struct sched_entity *se)
 {
-	if (se->sched_delayed)
+	if (se->ext.sched_delayed)
 		return false;
 
 	return !!se->on_rq;
