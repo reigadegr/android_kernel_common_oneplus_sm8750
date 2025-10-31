522c672bf0a3521bd4b29cd6011856cde87486bb
reigadegr <2722688642@qq.com>
2025-11-01 00:15:39 +0800

    Revert "[BSP.Kernel.Sched][1/1]{EXT/EXT-GKI:opensource回退}"
    This reverts commit 24c5973b9c3231a138fb630a0371c23494368895.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -190,8 +190,11 @@ static inline int idle_policy(int policy)
 
 static inline int fair_policy(int policy)
 {
+#ifdef CONFIG_HMBIRD_SCHED
+	return policy == SCHED_HMBIRD || policy == SCHED_NORMAL || policy == SCHED_BATCH;
+#else
 	return policy == SCHED_NORMAL || policy == SCHED_BATCH;
-
+#endif
 }
 
 static inline int rt_policy(int policy)
@@ -506,6 +509,12 @@ static inline void set_task_rq_fair(struct sched_entity *se,
 			     struct cfs_rq *prev, struct cfs_rq *next) { }
 #endif /* CONFIG_SMP */
 #else /* CONFIG_FAIR_GROUP_SCHED */
+#ifdef CONFIG_HMBIRD_SCHED
+static inline int sched_group_set_shares(struct task_group *tg, unsigned long shares)
+{
+	return 0;
+}
+#endif
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
 #else /* CONFIG_CGROUP_SCHED */
@@ -3600,5 +3609,8 @@ static inline bool cpu_busy_with_softirqs(int cpu)
 }
 #endif /* CONFIG_RT_SOFTIRQ_AWARE_SCHED */
 
+#ifdef CONFIG_HMBIRD_SCHED
+#include "hmbird.h"
+#endif
 
 #endif /* _KERNEL_SCHED_SCHED_H */
