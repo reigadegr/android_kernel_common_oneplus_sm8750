29d41fc24125e40b2f24e3890778c22fa6deb1b5
reigadegr <2722688642@qq.com>
2025-12-17 15:11:48 +0800

    Revert "ANDROID: Don't use current pointer for per-process dmabuf accounting"
    This reverts commit 13beada2dac38c2d7303f1700aa16fb5053b919d.

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1012,7 +1012,6 @@ struct mm_struct {
 		} lru_gen;
 #endif /* CONFIG_LRU_GEN */
 
-		struct task_dma_buf_info *dmabuf_info;
 		ANDROID_KABI_RESERVE(1);
 		ANDROID_BACKPORT_RESERVE(1);
 	} __randomize_layout;
