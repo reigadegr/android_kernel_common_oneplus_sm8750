b2ec6215220f185500c6f48125ad217b002cdfd7
reigadegr <2722688642@qq.com>
2025-12-17 15:11:45 +0800

    Revert "ANDROID: ABI: refresh, CRC fixups and allowed breaks"
    This reverts commit 3ba0c48b5ddf1faf98f9ddf83717a9b616128a28.

--- fs/exec.c
+++ fs/exec.c
@@ -67,10 +67,7 @@
 #include <linux/time_namespace.h>
 #include <linux/user_events.h>
 #include <linux/page_size_compat.h>
-
-#ifndef __GENKSYMS__
 #include <linux/dma-buf.h>
-#endif
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
