219e23c7e3121a49fcf30a2bae8113e6ef22205d
reigadegr <2722688642@qq.com>
2025-12-23 01:15:32 +0800

    Revert "mm: memcontrol: fix UAF in percpu_ref_get_many"
    This reverts commit 4a05d7958f37f5f39483fd1d92625023e3de9f6e.

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1943,6 +1943,4 @@ static inline void obj_cgroup_uncharge_zswap(struct obj_cgroup *objcg,
 }
 #endif
 
-void drain_obj_stock_all_cpus(struct obj_cgroup *objcg);
-
 #endif /* _LINUX_MEMCONTROL_H */
